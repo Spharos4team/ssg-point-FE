3ebf136e991f36462d9bbf135022fd2c3a876661
DESKTOP-42DEBT2\교육생58 <ksun2128@pukyong.ac.kr>
2023-08-21 20:28:40 +0900

    230821 cardmanage

--- app/(pages)/(mypage&mypoint)/myPoint/cardManage/page.tsx
+++ app/(pages)/(mypage&mypoint)/myPoint/cardManage/page.tsx
@@ -2,20 +2,24 @@ import RoundedButton from "@/components/atoms/Button/RoundedButton";
 import ButtonBasic from "@/components/atoms/Button/RoundedButton";
 import ButtonPrimary from "@/components/atoms/ButtonPrimary";
 import PointBox from "@/components/atoms/PointBox";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function cardManage() {
+  // @ts-ignore
   return (
     <>
       <h3 className="hidden">포인트 카드 관리</h3>
       <PointBox type="simpleCard" />
 
       {/* 카드 테이블 */}
-      <div>
-        <p>온라인 카드</p>
+      <div className="px-5 py-10">
+
+        <p className="font-medium text-[16px] leading-[26px] pb-[16px]">온라인 카드</p>
         {/* 온라인 카드 테이블 */}
-        <div>
-          <table>
-            <caption>온라인/모바일 카드</caption>
+        <div className="pb-10">
+          <table className="table-fixed w-full border-t-[1px] border-solid border-black">
+            <caption className="hidden absolute z-[-1] w-[1px] h-[1px] ">온라인/모바일 카드</caption>
             <colgroup>
               <col width={"40%"} />
               <col width={"auto"} />
@@ -23,49 +27,110 @@ export default function cardManage() {
             </colgroup>
             <thead>
               <tr>
-                <th>카드번호</th>
-                <th>발급처</th>
-                <th>발급일자</th>
+                <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">카드번호</th>
+                <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">발급처</th>
+                <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">발급일자</th>
               </tr>
             </thead>
             <tbody>
               {/* tr map */}
               <tr>
-                <td>9350-****-****-5220</td>
-                <td>신세계포인트닷컴</td>
-                <td> 2023-08-01</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">3591-****-****-5278</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">카카오페이</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]"> 2021-03-27</td>
               </tr>
               <tr>
-                <td>9350-****-****-6205</td>
-                <td>신세계포인트닷컴</td>
-                <td> 2023-07-27</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">9350-****-****-4246</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">신세계포인트닷컴</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]"> 2023-07-27</td>
+              </tr>
+              <tr>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">9350-****-****-8602</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">신세계포인트XSSG.COM</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]"> 2021-06-28</td>
+              </tr>
+              <tr>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">9350-****-****-4387</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">에브리데이(ON)</td>
+                <td className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]"> 2023-08-03</td>
               </tr>
             </tbody>
           </table>
         </div>
-
-        {/* 데이터가 없을 때 */}
-        <div>
-          <table>
-            <caption>온라인/모바일 카드</caption>
+        <p className="font-medium text-[16px] leading-[26px] pb-[16px] hidden">비즈 클럽 카드</p>
+          {/*<Link className="absolute right-2.5 top-[-37px] text-3 leading-5 font-[400]" href={""} alt={"카드 관리로 이동"}>카드 관리</Link>*/}
+          <table className="table-fixed w-full border-t-[1px] border-solid border-black">
+            <caption className="w-full table-fixed border-solid border-1 border-black">비즈 클럽 카드</caption>
             <colgroup>
               <col width={"40%"} />
               <col width={"auto"} />
               <col width={"81px"} />
             </colgroup>
             <thead>
+            <tr>
+              <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">카드번호</th>
+              <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">회사명</th>
+              <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">발급일자</th>
+            </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        {/*/!* 데이터가 없을 때 *!/*/}
+        {/*<div>*/}
+        {/*  <p className="font-medium text-[16px] leading-[26px] pb-[16px]">온라인 카드</p>*/}
+        {/*  <table className="relative pb-[40px]">*/}
+        {/*    <caption >온라인/모바일 카드</caption>*/}
+        {/*    <colgroup>*/}
+        {/*      <col width={"40%"} />*/}
+        {/*      <col width={"auto"} />*/}
+        {/*      <col width={"81px"} />*/}
+        {/*    </colgroup>*/}
+        {/*    <thead>*/}
+        {/*    <tr>*/}
+        {/*      <th>카드번호</th>*/}
+        {/*      <th>발급처</th>*/}
+        {/*      <th>발급일자</th>*/}
+        {/*    </tr>*/}
+        {/*    </thead>*/}
+        {/*    <tbody>*/}
+        {/*    /!* tr map *!/*/}
+        {/*    <tr>*/}
+        {/*      <td colSpan={3}>*/}
+        {/*        <div>*/}
+        {/*          <p className="after:content-[''] after:absolute after:top-0 after:left-1/2 after:w-12 after:h-12 after:bg-[url('/images/icon_nodata.png')] after:bg-no-repeat after:bg-[100%_auto]">*/}
+        {/*            보유하신 제휴 신용카드가 없습니다.*/}
+        {/*          </p>*/}
+        {/*        </div>*/}
+        {/*      </td>*/}
+        {/*    </tr>*/}
+        {/*    </tbody>*/}
+        {/*  </table>*/}
+        {/*</div>*/}
+
+
+          <p className="font-medium text-[16px] leading-[26px] pb-[16px]">제휴 신용카드</p>
+          <div className="relative pb-[40px]">
+          <table className="table-fixed w-full border-t-[1px] border-solid border-black">
+            <caption className="hidden absolute z-[-1] w-[1px] h-[1px]">제휴 신용카드</caption>
+            <colgroup>
+              <col width={"56%"} />
+              <col width={"auto"} />
+              <col width={"81px"} />
+            </colgroup>
+            <thead>
               <tr>
-                <th>카드번호</th>
-                <th>발급처</th>
-                <th>발급일자</th>
+                <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">카드번호</th>
+                <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">발급처</th>
+                <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">발급일자</th>
               </tr>
             </thead>
             <tbody>
               {/* tr map */}
               <tr>
-                <td colSpan={3}>
-                  <div>
-                    <p className="after:content-[''] after:absolute after:top-0 after:left-1/2 after:w-12 after:h-12 after:bg-[url('/images/icon_nodata.png')] after:bg-no-repeat after:bg-[100%_auto]">
+                <td className="text-[#767676] font-[400] align-middle text-center border-b-[1px] border-solid border-#e8e8e8 py-3 box-border text-[13px] leading-[21px] break-keep" colSpan={3}>
+                  <div className="py-[28px] ">
+                    <p className="relative pt-16 text-[13px] text-center text-[#767676] after:content-[''] after:absolute after:bg-[url('/images/icon_nodata.png')] after:bg-no-repeat after:top-0 after:left-1/2 after:w-12 after:h-12 after:bg-[100%_auto] after:translate-x-[-50%]">
+                    {/*<p className="after:content-[''] after:absolute after:top-0 after:left-1/2 after:w-12 after:h-12 after:bg-[url('/images/icon_nodata.png')] after:bg-no-repeat after:bg-[100%_auto] ">*/}
                       보유하신 제휴 신용카드가 없습니다.
                     </p>
                   </div>
@@ -73,27 +138,83 @@ export default function cardManage() {
               </tr>
             </tbody>
           </table>
+          </div>
+
+
+
+          <p className="font-medium text-[16px] leading-[26px] pb-[16px]">오프라인 카드</p>
+          <div className="relative">
+          <table className="table-fixed w-full border-t-[1px] border-solid border-black">
+            <caption className="hidden absolute z-[-1] w-[1px] h-[1px]">온라인/모바일 카드</caption>
+            <colgroup>
+              <col width={"40%"} />
+              <col width={"auto"} />
+              <col width={"81px"} />
+            </colgroup>
+            <thead>
+            <tr>
+              <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">카드번호</th>
+              <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">발급처</th>
+              <th className="font-[400] align-middle text-center border-b-[1px] border-solid border-[#e8e8e8] py-3 box-border text-[13px] break-keep leading-[21px]">발급일자</th>
+            </tr>
+            </thead>
+            <tbody>
+            {/* tr map */}
+            <tr>
+              <td className="text-[#767676] font-[400] align-middle text-center border-b-[1px] border-solid border-#e8e8e8 py-3 box-border text-[13px] leading-[21px] break-keep" colSpan={3}>
+                <div className="py-[28px]">
+                  <p className="relative pt-16 text-[13px] text-center text-[#767676] after:content-[''] after:absolute after:bg-[url('/images/icon_nodata.png')] after:bg-no-repeat after:top-0 after:left-1/2 after:w-12 after:h-12 after:bg-[100%_auto] after:translate-x-[-50%]">
+                    보유하신 오프라인 카드가 없습니다.
+                  </p>
+                </div>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+
+
+      </div>
+      {/*
+      <div>
+        <div>
+          <button/>
         </div>
       </div>
-
+       이런 구조가 됨
+      */}
       {/* 버튼 모음 */}
-      <div className="flex flex-col gap-2 pb-10 px-5">
-        <RoundedButton>신규 포인트카드 등록하기</RoundedButton>
-        <RoundedButton>임시 발급 카드 인증하기</RoundedButton>
-        <ButtonPrimary round="rounded-lg">
+      <div className="pl-5 pr-5 pb-10">
+        <RoundedButton className="mb-[15px]">신규 포인트카드 등록하기</RoundedButton>
+        <RoundedButton className="mb-[15px]">임시 발급 카드 인증하기</RoundedButton>
+        <RoundedButton className="" backgroundColor="primary">
           포인트 비밀번호 변경하기
-        </ButtonPrimary>
+        </RoundedButton>
       </div>
 
       {/* bottom banner */}
-      <div>
-        <p>
+      <div className="pt-10 pb-20 px-5 bg-[#f5f5f5]">
+        <p className="text-[18px] leading-[26px] mb-5">
           다양한 신세계포인트
           <br />
-          <strong>제휴 신용카드의 혜택</strong>을 확인하세요!
+          <strong className="font-[500] text-[#ea035c]">제휴 신용카드의 혜택</strong>을 확인하세요!
         </p>
-        <ul>
-          <li>{/* LinkImageWithFallback */}</li>
+        <ul className="list-none">
+          <li className="rounded-lg overflow-hidden">
+            {/* LinkImageWithFallback */}
+            {/*<Link href={""} title={"이벤트 페이지 이동"}/>*/}
+              {/* eslint-disable-next-line react/jsx-no-undef */}
+              <Image
+                src={"/images/ad_pointCard.jpg"}
+                alt={"SHINSEGAE POINT 다양한 카드로 즐기는 포인트 우대&middot;쇼핑 혜택, 신세계포인트 제휴신용카드 보기 &rarr;"}
+                width={750}
+                height={660}
+                style={{
+                  width: '100%',
+                  height: 'auto',
+                }}
+              />
+          </li>
         </ul>
       </div>
     </>
--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,10 +1,15 @@
+'use client'
 import { InputText } from "@/components/atoms/InputText";
-import LinkImageWithFallback from "@/components/ui/LinkImageWithFallback";
-import PrimaryButton from "@/components/ui/PrimaryButton";
 import Image from "next/image";
+import React, { useState } from 'react';
+import RoundedButton from "@/components/atoms/Button/RoundedButton";
+
 
 //TODO: 자동 로그인, 아이디 저장 기능 구현: 쿠키 활용할 것
-export default function login() {
+export default function Login() {
+
+  const [isChecked1, setIsChecked1] = useState<boolean>(false);
+  const [isChecked2, setIsChecked2] = useState<boolean>(false);
   const list_between =
     "after:content-[''] after:w-[1px] after:h-[13px] after:bg-gray-500 after:mt-[5px] after:mx-[16px]";
   return (
@@ -18,8 +23,7 @@ export default function login() {
           />
         </div>
         <div className="login_input_area space-y-5">
-          {/* FIXME: input 마다 다른 로직이 수행되어야 한다면 어떻게 할래?
-              TODO: InputText 대신 TextBox 사용 */}
+          {/* FIXME: input 마다 다른 로직이 수행되어야 한다면 어떻게 할래? */}
           <InputText
             type="email"
             title="로그인을 위해 아이디를 입력해주세요."
@@ -32,17 +36,34 @@ export default function login() {
           />
         </div>
 
+        <div > 
+          <div
+            className="login_check_box flex justify-start items-center mt-4 ml-2.5 "
+          >
+            <form className="remember_box flex items-center w-[50%]">
+              <input className={isChecked1 ? "bg-[url('/images/check_icon.png')] bg-[97%_auto] bg-no-repeat w-[22px] h-[22px] rounded-full border-2 border-gray-500 appearance-none bg-black" : "w-[22px] h-[22px] rounded-full border-2 border-gray-500 appearance-none"}
+              type="checkbox"
+              id="rememberId"
+              onChange={()=>setIsChecked1(!isChecked1)}
+
+              />
+              <label className="before:bg-[url('/images/check_icon.png')]" htmlFor="rememberId" >아이디 저장</label>
+            </form>
+            <form className="autoLogin_box flex items-center w-[50%]">
+              <input
+                type="checkbox"
+                id="autoLogin"
+                onChange={()=>setIsChecked2(!isChecked2)}
+                className={isChecked2 ? "bg-[url('/images/check_icon.png')] bg-[97%_auto] bg-no-repeat w-[22px] h-[22px] rounded-full border-2 border-gray-500 appearance-none bg-black" : "w-[22px] h-[22px] rounded-full border-2 border-gray-500 appearance-none"}
+              />
+              <label htmlFor="autoLogin" style={{ paddingLeft: '5px'}}>자동 로그인</label>
+          
+            </form>
+            </div>
+        </div>
+
         <div className="my-10">
-          {/* TODO: 컴포넌트 분리: innerText, onClick, border-radius */}
-          <button className="text-lg leading-7 font-bold text-center align-middle w-full px-2 py-3 rounded-full bg-gradient-primary">
-            로그인
-          </button>
-          {/* <PrimaryButton
-            btn_name="로그인"
-            on_click={
-              console.log("test");
-            }
-          /> */}
+          <RoundedButton className="" backgroundColor="primary">로그인</RoundedButton>
         </div>
 
         <ul className="text-center">
@@ -64,9 +85,25 @@ export default function login() {
         </ul>
       </div>
 
-      <div className="sns_list_box p-10 pb-20">
-        {/* <LinkImageWithFallback _href="" image={} name="네이버" fontHidden /> */}
+      <div className="sns_list_box p-10">
+        <h3>
+          간편 로그인
+        </h3>
+        <ul
+        className="sns_list flex justify-between items-center mt-5">
+          <li
+          className="naver relative w-12 h-12 bg-[url('/images/ico_sns.6ebacaac.png')] bg-no-repeat bg-[98%_auto] bg-[left_0px_top_-56px]"
+          >
+          </li>
+          <li
+          className="kakao relative w-12 h-12 bg-[url('/images/ico_sns.6ebacaac.png')] bg-[98%_auto] bg-no-repeat bg-[left_0px_top_-113px]"
+          ></li>
+          <li
+          className="apple relative w-12 h-12 bg-[url('/images/ico_sns.6ebacaac.png')] bg-no-repeat bg-[98%_auto] bg-[left_0px_top_-169px]"
+          ></li>
+        </ul>
       </div>
+
     </>
   );
 }
